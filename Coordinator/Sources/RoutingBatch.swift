b7dbeb986e05a75072e858e2229e661be09c3398
Andrii Vysotskyi <a.vysotskyi@icloud.com>
2022-05-22 22:59:19 +0200

    Refine routing batch behaviour

--- Coordinator/Sources/RoutingBatch.swift
+++ Coordinator/Sources/RoutingBatch.swift
@@ -19,19 +19,12 @@ public struct RoutingBatch {
                 assertionFailure("Unable to trigger route!")
                 return false
             }
-            let existingChildrenIdentifiers = Set(coordinator.children.map(ObjectIdentifier.init))
             let didTriggerRoute = route.execute(on: coordinator)
             guard didTriggerRoute else {
                 assertionFailure("Unable to trigger route!")
                 return false
             }
-            let newChildren = coordinator.children.filter { child in
-                !existingChildrenIdentifiers.contains(ObjectIdentifier(child))
-            }
-            if newChildren.count > 1 {
-                assertionFailure("More than one new child, this is not supported yet!")
-            }
-            nextCoordinator = newChildren.first
+            nextCoordinator = coordinator.children.first(where: route.isExecutable(on:))
         }
         return true
     }
@@ -42,6 +35,10 @@ public struct RoutingBatch {
 private extension RouteType {
 
     func execute(on coordinator: CoordinatorType) -> Bool {
-        return coordinator.router(for: Self.self)?.trigger(route: self) ?? false
+        coordinator.router(for: Self.self)?.trigger(route: self) ?? false
+    }
+
+    func isExecutable(on coordinator: CoordinatorType) -> Bool {
+        coordinator.router(for: Self.self) != nil
     }
 }
